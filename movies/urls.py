dfd92363e7c7b46f64356d3e00ce48fa3ccd97d0
Xac47 <hasbulat69@gmail.com>
2023-03-26 22:30:17 +0300

    add url adress in movies/urls.py name=list-category

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from movies import views
 
 urlpatterns = [
     path('', views.MoviesListView.as_view(), name='home'),
+    path('category/<slug:category>/', views.CategoryListView.as_view(), name='list-category'),
 ]
