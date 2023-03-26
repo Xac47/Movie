b6f78eaa158082d5f50977e8359f8155e2e343f8
Xac47 <hasbulat69@gmail.com>
2023-03-26 22:28:48 +0300

    add class in movies/views.py CategiryListView

--- movies/views.py
+++ movies/views.py
@@ -6,3 +6,13 @@ from movies.models import Movie
 
 class MoviesListView(ListView):
     model = Movie
+
+    def get_queryset(self):
+        return Movie.objects.filter(draft=False).order_by('-create_at')
+
+
+class CategoryListView(ListView):
+    model = Movie
+
+    def get_queryset(self):
+        return Movie.objects.filter(category__url=self.kwargs.get('category')).select_related('category')
