433fdef25a9629283549c6617ebb6b5ce2d35b29
Xac47 <hasbulat69@gmail.com>
2023-03-27 05:30:47 +0300

    add ReviewForm in file movies/forms.py

--- movies/forms.py
+++ movies/forms.py
@@ -1,1 +1,15 @@
 from django import forms
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        fields = ('name', 'email', 'text')
+        labels = {
+            'name': '',
+            'email': '',
+            'text': ''
+        }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control border"}),
+            "email": forms.EmailInput(attrs={"class": "form-control border"}),
+            "text": forms.Textarea(attrs={"class": "form-control border"})
+        }
