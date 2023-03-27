fc1608d42bc53cbbf29ddaf197be216ce2e2d580
Xac47 <hasbulat69@gmail.com>
2023-03-27 05:10:33 +0300

    add url in movies/urls.py MovieDetailView  name=movie

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from movies import views
 
 urlpatterns = [
     path('', views.MoviesListView.as_view(), name='home'),
-    path('category/<slug:category>/', views.CategoryListView.as_view(), name='list-category'),
+    path('search/', views.SearchView.as_view(), name='search'),
+    path('category/<slug:slug>/', views.CategoryListView.as_view(), name='list-category'),
+    path('movie/<slug:slug>/', views.MovieDetailView.as_view(), name='movie')
 ]
