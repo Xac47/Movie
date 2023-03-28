d20c0eca689c8297eb6954c714119fb22a0cd11a
Xac47 <hasbulat69@gmail.com>
2023-03-28 18:21:07 +0300

    add View SubscribeEmailView in contact/views.py

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from contact.forms import SubscribeEmailForm
+from contact.models import SubscribeEmail
+
+
+class SubscribeEmailView(CreateView):
+    model = SubscribeEmail
+    form_class = SubscribeEmailForm
+
+    def get_success_url(self):
+        return reverse('home')
