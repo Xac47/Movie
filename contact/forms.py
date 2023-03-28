30a3038b031f8740cc4132a7a3ff169c3dff0d93
Xac47 <hasbulat69@gmail.com>
2023-03-28 18:14:32 +0300

    add url adres config/urls.py

--- contact/forms.py
+++ contact/forms.py
@@ -1,1 +1,15 @@
 from django import forms
+
+from contact.models import SubscribeEmail
+
+
+class EmailForm(forms.ModelForm):
+    class Meta:
+        model = SubscribeEmail
+        fields = ('email',)
+        labels = {
+            'email': ''
+        }
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'editContent', 'placeholder': 'Enter your email...'})
+        }
