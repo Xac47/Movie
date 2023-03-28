142ee995df6bded948b3155d180c80c18f634d6e
Xac47 <hasbulat69@gmail.com>
2023-03-28 05:16:26 +0300

    add class MovieDetailView in file movies/views.py

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 
+from movies.forms import ReviewForm
 from movies.models import Movie
 
 
@@ -15,4 +18,43 @@ class CategoryListView(ListView):
     model = Movie
 
     def get_queryset(self):
-        return Movie.objects.filter(category__url=self.kwargs.get('category')).select_related('category')
+        return Movie.objects.filter(
+            Q(category__url=self.kwargs.get('slug')) &
+            Q(draft=False)
+        ).select_related('category')
+
+
+class SearchView(ListView):
+    model = Movie
+
+    def get_queryset(self):
+        return Movie.objects.filter(
+            Q(title__icontains=self.request.GET.get('q')) &
+            Q(draft=False)
+        ).order_by('-create_at')
+
+
+class MovieDetailView(FormMixin, DetailView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    slug_field = 'url'
+    form_class = ReviewForm
+
+    def get_success_url(self, **kwargs):
+        return self.get_object().get_absolute_url()
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.movie = self.get_object()
+        if self.request.POST.get('parent'):
+            self.object.parent_id = int(self.request.POST.get('parent'))
+        self.object.save()
+        return super().form_valid(form)
+
