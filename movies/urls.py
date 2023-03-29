3568c114ad81d3fd475b27f8773da21d8ddebf4e
Xac47 <hasbulat69@gmail.com>
2023-03-30 02:18:15 +0300

    add url adres name=actor

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.MoviesListView.as_view(), name='home'),
     path('search/', views.SearchView.as_view(), name='search'),
     path('category/<slug:slug>/', views.CategoryListView.as_view(), name='list-category'),
-    path('movie/<slug:slug>/', views.MovieDetailView.as_view(), name='movie')
+    path('movie/<slug:slug>/', views.MovieDetailView.as_view(), name='movie'),
+    path('actor/<str:name>/', views.ActorDetailView.as_view(), name='actor'),
 ]
