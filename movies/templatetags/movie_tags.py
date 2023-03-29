4323ace45de5bac908fc3a121d11c0f0106c73b5
Xac47 <hasbulat69@gmail.com>
2023-03-30 02:35:22 +0300

    update func last_movies add argument

--- movies/templatetags/movie_tags.py
+++ movies/templatetags/movie_tags.py
@@ -9,6 +9,6 @@ def get_categories():
     return Category.objects.all()
 
 @register.inclusion_tag('movies/include/tags/last_added.html')
-def last_movies():
-    movie_list = Movie.objects.filter(draft=False).order_by('-create_at')[:5]
+def last_movies(count=5):
+    movie_list = Movie.objects.filter(draft=False).order_by('-create_at')[:count]
     return {'movie_list': movie_list}
