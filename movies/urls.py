7eba666c4ae118579cc450b594fea4e39f05d779
Xac47 <hasbulat69@gmail.com>
2023-03-30 06:46:11 +0300

    add url adres name=filter

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,7 @@ from movies import views
 urlpatterns = [
     path('', views.MoviesListView.as_view(), name='home'),
     path('search/', views.SearchView.as_view(), name='search'),
+    path('filter/', views.FilterMovieView.as_view(), name='filter'),
     path('category/<slug:slug>/', views.CategoryListView.as_view(), name='list-category'),
     path('movie/<slug:slug>/', views.MovieDetailView.as_view(), name='movie'),
     path('actor/<str:name>/', views.ActorDetailView.as_view(), name='actor'),
