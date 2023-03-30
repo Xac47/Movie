f75e5a26d8b9b0392d495d94ed59111b1ccb0ef6
Xac47 <hasbulat69@gmail.com>
2023-03-31 00:33:14 +0300

    add url adres name=add_rating in movies/urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('filter/', views.FilterMovieView.as_view(), name='filter'),
     path('category/<slug:slug>/', views.CategoryListView.as_view(), name='list-category'),
     path('movie/<slug:slug>/', views.MovieDetailView.as_view(), name='movie'),
+    path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
     path('actor/<str:name>/', views.ActorDetailView.as_view(), name='actor'),
 ]
