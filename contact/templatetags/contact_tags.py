c680ba4a5e425946749edda85d7d1880cbb3559f
Xac47 <hasbulat69@gmail.com>
2023-04-04 21:18:58 +0300

    add function get_contact in file contact/templatetags/contact_tags.py

--- contact/templatetags/contact_tags.py
+++ contact/templatetags/contact_tags.py
@@ -1,9 +1,14 @@
 from django import template
 
 from contact.forms import SubscribeEmailForm
+from contact.models import Contact
 
 register = template.Library()
 
 @register.simple_tag()
 def get_subscribe_email_form():
     return SubscribeEmailForm()
+
+@register.simple_tag()
+def get_contacts():
+    return Contact.objects.first()
