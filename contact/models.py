bfa59f4efb971d94e19ec079c007f874f9b059f9
Xac47 <hasbulat69@gmail.com>
2023-04-04 21:10:55 +0300

    add model Contact in file contact/models.py

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class SubscribeEmail(models.Model):
     email = models.EmailField(max_length=90)
     create_at = models.DateTimeField(auto_now_add=True)
@@ -11,3 +12,21 @@ class SubscribeEmail(models.Model):
     def __str__(self):
         return f'{self.email} - {self.create_at}'
 
+
+class Contact(models.Model):
+    title = models.CharField('Заголовок', max_length=30)
+    desc = models.CharField('Описание', max_length=255)
+    email = models.EmailField(unique=True)
+    location = models.CharField('Локация', max_length=30)
+
+    google_plus = models.URLField()
+    twitter = models.URLField()
+    facebook = models.URLField()
+    dribbble = models.URLField()
+
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contact'
+
+    def __str__(self):
+        return self.title
