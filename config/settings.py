4e9d49613319d3f2b64fd4d6f35bda54daff474c
Xac <110842560+Xac47@users.noreply.github.com>
2023-04-07 15:27:15 +0300

    Update settings.py
    add config email, celery, redis in file config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -216,8 +216,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pochtat808@gmail.com'
-EMAIL_HOST_PASSWORD = 'hshgswnohjoepzbn'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 
 # redis
 
